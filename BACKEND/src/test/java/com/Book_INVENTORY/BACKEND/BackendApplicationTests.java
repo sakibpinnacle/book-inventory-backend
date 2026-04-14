3a917aaec66ca93f896b84bd3f6e77d9e9467dc0
sakib880git <sakibsheikh2950@gmail.com>
2026-04-14 11:32:07 +0530

    Initial commit

---
+++ BACKEND/src/test/java/com/Book_INVENTORY/BACKEND/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Book_INVENTORY.BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
